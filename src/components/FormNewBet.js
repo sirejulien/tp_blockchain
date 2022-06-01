a3a1b4607fa86b1d029620bd56f43039a3f315ec
sirejulien <sire.jul@gmail.com>
2022-06-01 14:50:37 +0200

    formulaire

---
+++ src/components/FormNewBet.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class FormNewBet extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          arbitre: "",
+          parieurA:"",
+          parieurB:"",
+          montant:0,
+          sujet:""
+      };
+  
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+  
+    handleInputChange(event) {
+      const target = event.target;
+      const value = target.value;
+      const name = target.name;
+  
+      this.setState({
+        [name]: value
+      });
+    }
+
+    handleSubmit(event) {
+        alert('formulaire envoyé');
+        event.preventDefault();
+      }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+            <label>Arbitre :
+            <input name="arbitre" type="text" value={this.state.arbitre} onChange={this.handleInputChange}/>
+            </label><br/>
+            <label>Parieur A :
+            <input name="parieurA" type="text" value={this.state.parieurA} onChange={this.handleInputChange}/>
+            </label><br/>
+            <label>Parieur B :
+            <input name="parieurB" type="text" value={this.state.parieurB} onChange={this.handleInputChange}/>
+            </label><br/>
+            <label>Montant :
+            <input name="montant" type="number" value={this.state.montant} onChange={this.handleInputChange}/>
+            </label><br/>
+            <label>Sujet :
+            <textarea name="sujet" cols="100" rows="10" value={this.state.sujet} onChange={this.handleInputChange}></textarea>
+            </label><br/>
+            <input type="submit" value="Create Bet"/>
+        </form>
+      );
+    }
+  }
+
+  export default FormNewBet;
--- src/pages/NewBet.js
+++ src/pages/NewBet.js
@@ -1,23 +1,12 @@
 import React from 'react';
+import FormNewBet from '../components/FormNewBet';
 
 //page d'accueil, affiche un message de bienveu
 const NewBet = () =>{
   return (
     <div>
       <h2>New Bet</h2>
-      <form id="myForm">
-            <label for="arbitre">Arbitre :</label>
-            <input id="arbitre" name="arbitre" value=""/> <br/>
-            <label for="parieurA">Parieur A :</label>
-            <input id="parieurA" name="parieurA" value=""/><br/>
-            <label for="parieurB">Parieur B :</label>
-            <input id="parieurB" name="parieurB" value=""/><br/>
-            <label for="montant">Montant :</label>
-            <input type="number" id="montant" name="montant" value=""/><br/>
-            <label for="sujet">Sujet :</label>
-            <textarea id="sujet" name="sujet" cols="100" rows="10"></textarea><br/>
-            <input type="submit" value="Create Bet"/>
-          </form>
+      <FormNewBet />
     </div>
   );
 }
